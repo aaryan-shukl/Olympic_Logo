23a14cbdd865bedb90f549b948d097436da6d67a
Aaryan Shukla <78203854+aaryan-shukl@users.noreply.github.com>
2021-02-19 19:51:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+var canvas = document.getElementById("canvas");
+ctx = canvas.getContext("2d");
+
+canvas.addEventListener("mousedown", draw_logo);
+
+function draw_logo(e) {
+    LogoX = e.clientX - canvas.offsetLeft;
+	LogoY = e.clientY - canvas.offsetTop;
+    Logo_structure(LogoX, LogoY);
+}
+function Logo_structure(LogoX, LogoY) {
+    ctx.beginPath();
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 5;
+    ctx.arc(200, 200, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 5;
+    ctx.arc(250, 200, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 5;
+    ctx.arc(300, 200, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle = "yellow";
+    ctx.lineWidth = 5;
+    ctx.arc(225, 300, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle = "green";
+    ctx.lineWidth = 5;
+    ctx.arc(275, 300, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+}
